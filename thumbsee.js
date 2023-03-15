fc35cb54012b7efe06dc0289ed323d6c6ac2c727
treevar <user0@lfod.top>
2023-03-15 18:48:34 -0500

    Clean up code

--- thumbsee.js
+++ thumbsee.js
@@ -16,97 +16,78 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const DELAY_TIME = 500;
-const DELAY_TIMEOUT = 10000;
-const TIMEOUT_STEPS = DELAY_TIMEOUT / DELAY_TIME;
+const DELAY_TIME = 200;
+const elemQS = "#main-content > div > .card";//selector for element we need to insert before
 
-let timeoutTick = 0;
-let rended = false;
-function tick(){
-    return ++timeoutTick > TIMEOUT_STEPS;
+//Returns whether the query selctor qs is found within the document
+function elemExits(qs){
+    return document.querySelector(qs) != null;
 }
 
-function loadChild(main, data){
-    let child = null;
-    main = document.getElementById("main-content");
-    for(let i in main.children[0].children){
-        if(main.children[0].children[i].className == "card"){ child = main.children[0].children[i]; break; }
-    }
-    if(child == null){
-        //if(document.getElementById("vjs_video_3_html5_api")){ return; }//we on a video page
-        if(main.children[1] && main.children[1].className != "section card-stack file-page__download"){ rended = true; return; }//we are not on a page where this works
-        console.log("ThumbSee: Child was null");
-        if(tick()){ timeoutTick = 0; rended = true; return; }
-        setTimeout(()=>{ loadChild(main, data); }, DELAY_TIME);
-    }
-    else{
-        timeoutTick = 0;
-        let startI = data.indexOf("https://", data.indexOf("https://thumbnails.odycdn.com") + 30);
-        let url = data.substring(startI, data.indexOf('"', startI));//get thumbnail url
-        let sec = document.getElementById("thumbseeImageSection");
-        if(sec == null){
-            sec = document.createElement('section');
-            sec.id = "thumbseeImageSection";
-            sec.className = "card";
-            child.parentNode.insertBefore(sec, child);
-        }
-        sec.style = "margin-bottom: 10px; text-align: center;";
-        let img = document.getElementById("thumbseeImageMain");
-        if(img == null){
-            img = document.createElement('img');
-            sec.appendChild(img);
-        }
-        img.className = "media__thumb card__first-pane";
-        img.style = "max-width:60%;height:auto;";
-        img.src = url;
-        img.id = "thumbseeImageMain";
-        rended = true;
-        //sec.textContent = "<img class='media__thumb card__first-pane' style='max-width:60%;height:auto;' src='" + url + "' id=thumbseeImageMain>";
-    }
+//Checks every timeout ms for whether the given query selector is found in the document
+//Returns a promise that resolves when the given query selector is found
+//  note: if the query selector is never found then this doesn't resolve
+function waitFormElem(querySelector, timeout){
+    return new Promise (function(resolve, reject){
+        const interID = setInterval(function(){
+            if(elemExits(querySelector)){ clearInterval(interID); resolve(); }
+        }, timeout);
+         
+    });
 }
 
-function loadMain(data){
-    let main = document.getElementById("main-content");
-    if(main == null){
-        console.log("ThumbSee: main was null, page is probably loading still");
-        if(tick()){ timeoutTick = 0; console.log("Tick expire"); rended = true; return; }
-        setTimeout(()=>{
-            loadMain(data);
-        }, DELAY_TIME);
+
+//Adds the thumbnail image to the page
+function loadThumbnail(data){
+    let child = document.querySelector(elemQS);
+    let startI = data.indexOf("https://", data.indexOf("https://thumbnails.odycdn.com") + 30);
+    let url = data.substring(startI, data.indexOf('"', startI));//get thumbnail url
+    let sec = document.getElementById("thumbseeImageSection");
+    if(sec == null){//if we havent made the section yet
+        sec = document.createElement('section');
+        sec.id = "thumbseeImageSection";
+        sec.className = "card";
+        child.parentNode.insertBefore(sec, child);
     }
-    else{
-        timeoutTick = 0;
-        loadChild(main, data);
+    sec.style = "margin-bottom: 10px; text-align: center;";
+    let img = document.getElementById("thumbseeImageMain");
+    if(img == null){//if we havent already loaded the image
+        img = document.createElement('img');
+        sec.appendChild(img);
     }
+    img.className = "media__thumb";
+    img.style = "max-width:60%;height:auto;";
+    img.src = url;
+    img.id = "thumbseeImageMain";
+    rended = true;
 }
 
+//Retuns a promise that resolves in ms milliseconds
 function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-   }
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function start(){
     if(window.location.href.match(/^.*:\/\/odysee.com\/@.*\/.*$/)){//are we on a proper page?
-        console.log("ThumbSee: Started");
+        //console.log("ThumbSee: Started");
+        await waitFormElem(elemQS, DELAY_TIME);//wait until that element is loaded
+        if(document.querySelector("svg.icon.icon--Download") == null){ return; }//are we on a page with a download button?
+        //console.log("Loaded");
         fetch(window.location.href).then(function(res){
             return res.text();
 
         }).then((data) => {
-            loadMain(data);
+            loadThumbnail(data);
         });
     }
 
 }
+
 let curUrl = null;
-async function main(){
-    while(true){
-        if(curUrl != window.location.href){//if we clicked on something
-            curUrl = window.location.href;
-            rended = false;
-        }
-        if(rended == false){
-            setTimeout(() => {start();}, DELAY_TIME);//call start in a bit, we want the page to load first
-        }
-        await sleep(DELAY_TIME);
+//whenever the URL changes, call start
+const mainInterval = setInterval(function(){
+    if(curUrl != window.location.href){
+        curUrl = window.location.href;
+        start();
     }
-}
-main();
+}, DELAY_TIME);
