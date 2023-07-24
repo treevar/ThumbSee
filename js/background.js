6cc1161e05ea38aafd66e6f97972c516fd3438d7
treevar <treevar@lfod.top>
2023-07-24 18:10:32 -0500

    Update file structure

---
+++ js/background.js
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * * * * * *\
+ *  ThumbSee - SEE the THUMBnails :) *
+ *      Background script            *
+ *  Copyright (C) 2023 treevar       *
+\* * * * * * * * * * * * * * * * * * */
+
+browser.runtime.onInstalled.addListener((details) => {
+  // Check if the extension was installed
+  if (details.reason == "install") {
+    setToStorage("imageScale", 60);
+  }
+});
