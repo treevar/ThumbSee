8dfa5607da9dfa062a1666300a961a0870af758f
treevar <treevar@lfod.top>
2023-08-07 15:16:19 -0500

    Move DL btn by others

--- js/thumbsee.js
+++ js/thumbsee.js
@@ -22,6 +22,7 @@ const DELAY_TIME = 200;
 const TICKOUT = 10 * (1000 / DELAY_TIME);//Seconds * (ticks per second)
 const ELEM_QS = "#main-content > div > .card";//selector for element we need to insert before
 const DL_BUTTON_QS = "button[title=\"Download\"]";//selector used for finding download button
+const BTN_GROUP_QS = ".claim-preview__actions";//selector for group of buttons (follow/join buttons)
 
 //Returns whether the query selector qs is found within the document
 function elemExists(qs) {
@@ -97,7 +98,16 @@ function getImageUrl() {
 async function start() {
     //waits for DL button to be loaded and then loads the image (if an image url was found)
     waitForElem(DL_BUTTON_QS, DELAY_TIME, TICKOUT).then(() => {
-        getImageUrl().then((url) => { loadThumbnail(url); }, (err) => { console.log("Thumbsee: " + err); });
+        getImageUrl().then((url) => { 
+            loadThumbnail(url); 
+            //Move download button by the other buttons (follow/join)
+            let dlBtn = document.querySelector(DL_BUTTON_QS);
+            let btnGroup = document.querySelector(BTN_GROUP_QS);
+            btnGroup.prepend(dlBtn);
+            //Remove original download button section
+            let ogBtnSection = document.querySelector("#main-content > div > .card:not(#thumbseeImageSection)");
+            ogBtnSection.style.display = "none";
+        }, (err) => { console.log("Thumbsee: " + err); });
     }, ()=>{});
 }
 
